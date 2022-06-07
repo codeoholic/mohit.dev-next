111fe76ab706339de7eca6c09f17d296c4e429e5
codeoholic <mohit.tyagi@protonmail.com>
2022-06-07 23:30:07 +0530

    feat: home ui'

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
 import sanity from "../sanity"
 
-const Home = () => {
+const Home = ( props ) => {
+
+	const {
+
+		posts
+
+	} = props
 
 	return (
 
-		<div>I am home</div>
+		<div className="bg-slate-200 w-screen h-full p-2.5 md:p-0">
+			<div className="container mx-auto p-5 max-w-3xl bg-slate-50 shadow-md rounded">
+				{
+
+					posts.map( ( value, index ) => {
+
+						return(
+
+							<div key={ "post-" + index }>
+								<div className="text-3xl text-slate-900 font-bold">{ value.title }</div>
+								<div className="text-base text-slate-900">{ value.description }</div>
+							</div>
+
+						)
+					})
+
+				}
+			</div>
+		</div>
 
 	)
 
@@ -12,13 +36,13 @@ const Home = () => {
 
 export const getServerSideProps = async () => {
 
-	const post = await sanity.fetch(`*[_type == "post"]`)
-	console.log( post )
+	const posts = await sanity.fetch(`*[_type == "post"]`)
+	console.log( posts )
   	return {
 
   		props: {
 
-  			post
+  			posts
 
   		}
 
