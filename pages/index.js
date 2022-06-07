6d7e6dcd58626f62b1c994938032680cf008a47b
codeoholic <mohit.tyagi@protonmail.com>
2022-06-07 23:43:48 +0530

    feat: home ui

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import sanity from "../sanity"
 
 const Home = ( props ) => {
@@ -12,21 +13,28 @@ const Home = ( props ) => {
 
 		<div className="bg-slate-200 w-screen h-full p-2.5 md:p-0">
 			<div className="container mx-auto p-5 max-w-3xl bg-slate-50 shadow-md rounded">
+				<div className="grid gap-2.5">
 				{
 
 					posts.map( ( value, index ) => {
 
 						return(
 
-							<div key={ "post-" + index }>
-								<div className="text-3xl text-slate-900 font-bold">{ value.title }</div>
+							<div key={ "post-" + index } className="border rounded p-5 border-slate-200 shadow">
+								<Link href={ `/${ value.slug.current }` } passHref>
+									<a>
+										<div className="text-xl text-slate-900 font-bold">{ value.title }</div>
+									</a>
+								</Link>
 								<div className="text-base text-slate-900">{ value.description }</div>
 							</div>
 
 						)
+
 					})
 
 				}
+				</div>
 			</div>
 		</div>
 
@@ -37,7 +45,6 @@ const Home = ( props ) => {
 export const getServerSideProps = async () => {
 
 	const posts = await sanity.fetch(`*[_type == "post"]`)
-	console.log( posts )
   	return {
 
   		props: {
