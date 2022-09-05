a1dbbec35f076da488297686109ecbee1ff5cd47
Mohit Tyagi <mohit@hey.com>
2022-09-05 22:39:54 +0530

    feat: nextjs init

--- pages/next-js/index.js
+++ pages/next-js/index.js
@@ -1,15 +1,31 @@
+import sanity from "../../sanity"
+
 import Footer from "../../components/footer"
 import Header from "../../components/header"
 import MetaTags from "../../components/meta-tags"
 
-const NextJSIndex = () => {
+import { PortableText } from "@portabletext/react"
+
+const NextJSIndex = ( props ) => {
+
+    const {
+
+		body,
+		description,
+		meta_image,
+		publish_time,
+		title,
+
+	} = props.nextJS
+
+    console.log( props.nextJS )
 
     return (
 
         <>
             <MetaTags
-				title="Tecnocrat"
-				description={ "Using this platform I will be sharing my experiences on the technology that I explore. I might occasionally or more frequenty post my non-technological opinions too." }
+				title={ title }
+				description={ description }
 				image={ "https://mohit.sgp1.cdn.digitaloceanspaces.com/mohit.dev/assets/mohit.dev.webp" }
 			/>
 			<div className="bg-slate-200 dark:bg-slate-800 w-screen h-full">
@@ -18,7 +34,10 @@ const NextJSIndex = () => {
 				/>
                 <div className="mt-5 p-2.5 md:p-0">
 					<div className="container mx-auto p-5 max-w-3xl bg-slate-50 shadow-md rounded dark:bg-slate-800 dark:border dark:border-slate-300">
-                        <h1 className="text-2xl text-slate-900 dark:text-slate-100">Introduction to NextJS</h1>
+                        <h1 className="text-3xl text-slate-900 font-bold dark:text-slate-50">{ title }</h1>
+                        <div className="text-slate-700 mt-5 text-lg dark:text-slate-200 w-full prose prose-p:mt-0 prose-p:mb-2.5 max-w-none leading-normal prose-code:before:content-none prose-code:after:content-none prose-code:mt-2.5 prose-strong:dark:text-slate-200">
+                            <PortableText value={ body } />
+                        </div>
                     </div>
                 </div>
                 <Footer/>
@@ -26,6 +45,22 @@ const NextJSIndex = () => {
         </>
 
     )
+
+}
+
+export const getServerSideProps = async () => {
+
+	const nextJS = await sanity.fetch(`*[_type == "next-js" && slug.current == "introduction-to-nextjs"][0]{ _id, title, description, meta_image, publish_time, slug, body }`)
+	console.log( nextJS )
+  	return {
+
+  		props: {
+
+            nextJS
+
+  		}
+
+  	}
 
 }
 
