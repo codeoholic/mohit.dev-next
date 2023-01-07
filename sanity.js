aa4fec7c03c2c1e397794a099d4b20511126e7e8
Mohit Tyagi <mohit@hey.com>
2023-01-07 17:00:17 +0530

    feat: remove fonts

--- sanity.js
+++
@@ -1,9 +0,0 @@
-import sanityClient from '@sanity/client'
-
-export default sanityClient({
-	
-	dataset: process.env.SANITY_PROJECT_DATASET,
-	projectId: process.env.SANITY_PROJECT_ID,
-	useCdn: true,
-
-})
