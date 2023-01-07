ce2ec2e8203ce91e2dc3a615b46278017575b99e
Mohit Tyagi <mohit@hey.com>
2023-01-07 18:44:35 +0530

    feat: dark mode

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,8 @@ const Home = ( props ) => {
 				/>
 				<div className="container mx-auto px-5 lg:px-0 lg:max-w-4xl">
 					<div className="mt-5">
-						<h1 className="font-semibold text-zinc-800 text-xl">About</h1>
-						<p className="text-zinc-800 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<h1 className="font-semibold text-zinc-800 text-xl dark:text-white">About</h1>
+						<p className="text-zinc-800 text-lg dark:text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 					</div>
 					<ReadingList />
 				</div>
