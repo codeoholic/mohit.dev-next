dbfedba69c39ec2b718b14712f3bea22d4b2c462
Mohit Tyagi <mohit@hey.com>
2023-01-07 17:10:36 +0530

    feat: reading list component

---
+++ components/reading-list/ReadingList.js
@@ -0,0 +1,43 @@
+const { BOOKS } = require("./books")
+console.log( BOOKS )
+
+const ReadingList = () => {
+
+    return (
+
+        <div className="mt-5">
+            <h1 className="font-semibold text-zinc-800 text-xl">Reading List - 2023</h1>
+            <ol className="list-decimal pl-5 mt-2.5">
+            {
+
+                BOOKS.map( ( book, index ) => {
+                    
+                    return (
+                        
+                        <li key={ "book-" + index }>
+                            <div className="flex grid grid-cols-4 gap-5 items-center">
+                                <div>{ book.name }</div>
+                                <div>{ book.author }</div>
+                                <div className="relative">
+                                    <div className="bg-blue-600/40 w-full h-3 rounded-full"></div>
+                                    <div className={`bg-blue-600 w-[${ book.completion_percentage }%] h-3 rounded-full absolute top-0 left-0`}></div>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <div className="text-sm">{ book.completion_percentage !== 100 ? book.completion_percentage +"% complete" : "completed" }</div>
+                                </div>
+                            </div>
+                        </li>
+
+                    )                    
+
+                })
+
+            }
+            </ol>
+        </div>
+
+    )
+
+}
+
+export default ReadingList
---
+++ components/reading-list/books.js
@@ -0,0 +1,7 @@
+export const BOOKS = [{
+
+    name: "Animal Farm",
+    author: "George Orwell",
+    completion_percentage: 22
+
+}]
